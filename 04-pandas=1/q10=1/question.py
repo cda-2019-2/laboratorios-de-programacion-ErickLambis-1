e2773f3a40917ebb62d0e99bbb0c29cd58abcced
ErickLambis <57542392+ErickLambis@users.noreply.github.com>
2020-03-08 22:09:19 -0500

    Update question.py

--- 04-pandas=1/q10=1/question.py
+++ 04-pandas=1/q10=1/question.py
@@ -21,3 +21,7 @@ df10 = pd.read_csv("./q10=1/tbl2.tsv", sep="\t")
 df10 = df10.sort_values(by=['_c5a'])
 df10['_c5a'] = df10['_c5a'].apply(str) 
 df10.head()
+df10['comb'] = df10['_c5a'] + ":" + df10['_c5b'].map(str)
+df10 = df10.groupby('_c0')['comb'].apply(list).reset_index(name='lista')
+df10['lista'] = [','.join(map(str, l)) for l in df10['lista']]
+df10.head()
